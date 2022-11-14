5b1af06af0d422a7771d474a99c040e5261239a5
oywq2020 <2603321762@qq.com>
2022-11-14 15:24:01 +0800

    complete mysql tool customizing

---
+++ Assets/Script/Login/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginController : MonoBehaviour
+{
+    private InputField _nameInput;
+    private InputField _passwordInput;
+   
+    void Start()
+    {
+        //get input
+        _nameInput = transform.Find("InputName").GetComponent<InputField>();
+        _passwordInput = transform.Find("InputPassword").GetComponent<InputField>();
+        
+        //register button event
+        transform.Find("LoginBtn").GetComponent<Button>().onClick.AddListener(Login);
+        transform.Find("EnrollBtn").GetComponent<Button>().onClick.AddListener(Enroll);
+    }
+
+    void Login()
+    {
+        
+    }
+
+    void Enroll()
+    {
+        
+    }
+}
