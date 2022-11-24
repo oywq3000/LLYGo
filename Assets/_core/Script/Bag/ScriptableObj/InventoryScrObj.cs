a73abc9067fa9cfa605ddb0e92c1035832798c2f
oywq2020 <2603321762@qq.com>
2022-11-24 09:56:00 +0800

    prepare for merge to master

---
+++ Assets/_core/Script/Bag/ScriptableObj/InventoryScrObj.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Inventory",menuName = "Inventory/New Inventory")]
+public class InventoryScrObj : ScriptableObject
+{
+   //bag list
+   public List<AbstractItemScrObj> itemList = new List<AbstractItemScrObj>();
+
+   public int maxCount = 13;
+}
---
+++ Assets/_core/Script/Bag/ScriptableObj/Item/PostionItem.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace _core.Script.Bag.ScriptableObj.Item
+{
+    public class NormalItem : AbstractItemScrObj
+    {
+        public int count;
+    }
+}
