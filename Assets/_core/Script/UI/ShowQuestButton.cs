e20690b1561bb820e42b613000c02be55b1e8eaa
oywq2020 <2603321762@qq.com>
2022-12-10 14:58:57 +0800

    Add some Ui

---
+++ Assets/_core/Script/UI/ShowQuestButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DialogueQuests;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowQuestButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool swift = true;
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(() =>
+        {
+            QuestPanel.Get().TogglePanel();
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
