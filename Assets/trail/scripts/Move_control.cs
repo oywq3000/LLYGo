342353ce5bd1e28bf1d770fc07f595798eaa6ba5
oywq2020 <2603321762@qq.com>
2022-12-10 21:24:56 +0800

    prepare for push to remote

---
+++ Assets/trail/scripts/Move_control.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_control : MonoBehaviour
+{
+    public Animator animator;
+
+    public Rigidbody rigid_body;
+
+
+    public float speed = 8.0F;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+
+
+
+
+    void Start()
+    {
+        this.controller = GetComponent<CharacterController>();
+    }
+    void Update()
+    {
+        float offset_vertical = Input.GetAxis("Vertical");
+        float offset_horizontal = Input.GetAxis("Horizontal");
+
+
+        if (offset_vertical == 0 && offset_horizontal == 0)
+        {
+            this.animator.SetBool("is_run", false);
+            this.animator.SetBool("is_idle", true);
+
+        }
+        else
+        {
+            this.animator.SetBool("is_run", true);
+            this.animator.SetBool("is_idle", false);
+
+
+
+
+            this.moveDirection = new Vector3(-Input.GetAxis("Horizontal"), 0, -Input.GetAxis("Vertical"));
+
+            //this.transform.TransformDirection(this.moveDirection.normalized);
+
+            //this.moveDirection = transform.TransformDirection(this.moveDirection);
+
+
+            this.moveDirection *= this.speed;
+            //this.moveDirection.y -= gravity * Time.deltaTime;
+            this.transform.position += this.moveDirection;
+
+            transform.rotation = Quaternion.LookRotation(this.moveDirection);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.animator.SetBool("is_jump", true);
+            this.rigid_body.AddForce(Vector3.up*220);
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            this.animator.SetBool("is_jump", false);
+        }
+
+
+    }
+
+}
---
+++ Assets/trail/scripts/Trail_manager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trail_manager : MonoBehaviour
+{
+
+    [Header("target SkinnedMeshRenderer")]
+    public GameObject game_obj_target;
+
+    //The position of the previous target----------之前目标物体的位置
+    private Vector3 v3_position_game_obj_target_before;
+
+    [Header("how many trails-------定义多少个尾巴")]
+    public int trail_count;
+
+    [Header("The initial transparency of the tail------尾巴的初始透明度")][Range(0f, 1f)]
+    public float trail_alpha;
+
+    [Header("The time between each tail, in seconds---------每个尾巴间隔的时间，单位秒")]
+    public float trail_interval_time;
+
+    [Header("The speed at which each tail disappears------每个尾巴消失的速度")]
+    public float trail_disappear_speed;
+
+    [Header("Trail Color")]
+    [ColorUsageAttribute(true, true, 0f, 8f, 0.125f, 3f)]
+    public Color color_trail;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (this.trail_count > 0&&this.game_obj_target!=null)
+        {
+            //Generate trail objects--------生成trail物体
+            for (int i = 0; i < this.trail_count; i++)
+            {
+                GameObject trail = new GameObject("trail" + i);
+                trail.transform.SetParent(this.transform);
+                trail.AddComponent<MeshFilter>();
+                trail.AddComponent<MeshRenderer>();
+                trail.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off; //no shadow
+
+                Material mat = new Material(Shader.Find("EasyGameStudio/trail"));
+                mat.SetTexture("main_texture", this.game_obj_target.GetComponent<SkinnedMeshRenderer>().material.mainTexture);
+                mat.SetColor("color_fresnel_emission", this.color_trail);
+                trail.GetComponent<MeshRenderer>().material = mat;
+                Trail_control trail_control = trail.AddComponent<Trail_control>();
+
+                trail.SetActive(false);
+            }
+
+            StartCoroutine(this.trail_start());
+        }
+
+    }
+
+    //The coroutine starts to make the tail----------协程开始制作尾巴
+    IEnumerator trail_start()
+    {
+        while (true) 
+        {
+            for (int i = 0; i < this.trail_count; i++)
+            {
+
+                //If the position does not change, the tail will not be set--------位置没有变的话 就不设置尾巴
+                if (this.v3_position_game_obj_target_before!= this.game_obj_target.transform.position)
+                {
+                    GameObject trail = this.transform.GetChild(i).gameObject;
+                    trail.transform.position = this.game_obj_target.transform.position;
+                    trail.transform.rotation = this.game_obj_target.transform.rotation;
+                    if (trail.activeSelf == false)
+                        trail.SetActive(true);
+                    trail.GetComponent<Trail_control>().init(this.trail_disappear_speed, this.game_obj_target.GetComponent<SkinnedMeshRenderer>(), this.trail_alpha);               
+                }
+                this.v3_position_game_obj_target_before = this.game_obj_target.transform.position;
+
+                yield return new WaitForSeconds(this.trail_interval_time);
+            }
+        }
+    }
+}
